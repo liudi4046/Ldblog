82a0adc635fd61c86b4466396b69b20effdbdf1f
liudi <liudi404637575@gmail.com>
2023-06-02 20:35:15 +0800

    update

--- src/pages/home/components/BlogCard.tsx
+++ src/pages/home/components/BlogCard.tsx
@@ -1,10 +1,5 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-} from "@mui/material";
+import { Button, CardActions, CardContent, Typography } from "@mui/material";
+import { Card } from "./Card";
 import { BlogCardInfo } from "../../../types";
 import { useNavigate } from "react-router-dom";
 import moment from "moment-timezone";
@@ -21,42 +16,68 @@ export default function BlogCard({
     .tz(BlogCardInfo.created_at, "Asia/Shanghai")
     .format("MM-DD HH:mm");
   return (
-    <Card
-      sx={{
-        maxWidth: 345,
-        flexGrow: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-    >
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          className="text-center"
-        >
-          {BlogCardInfo.title}
-        </Typography>
-        <Typography variant="body2" textAlign={"center"} marginBottom={3}>
-          {formattedDate}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {BlogCardInfo.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button
-          sx={{
-            marginLeft: "auto",
-          }}
-          size="small"
-          onClick={viewBlogDetail}
-        >
-          Read More
-        </Button>
-      </CardActions>
+    <Card>
+      <Typography
+        gutterBottom
+        variant="h5"
+        component="div"
+        className="text-center"
+      >
+        {BlogCardInfo.title}
+      </Typography>
+      <Typography variant="body2" textAlign={"center"} marginBottom={3}>
+        {formattedDate}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {BlogCardInfo.description}
+      </Typography>
+
+      <Button
+        sx={{
+          marginLeft: "auto",
+        }}
+        size="small"
+        onClick={viewBlogDetail}
+      >
+        Read More
+      </Button>
     </Card>
+    // <Card
+    //   sx={{
+    //     maxWidth: 345,
+    //     flexGrow: 1,
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     justifyContent: "space-between",
+    //   }}
+    // >
+    //   <CardContent>
+    //     <Typography
+    //       gutterBottom
+    //       variant="h5"
+    //       component="div"
+    //       className="text-center"
+    //     >
+    //       {BlogCardInfo.title}
+    //     </Typography>
+    //     <Typography variant="body2" textAlign={"center"} marginBottom={3}>
+    //       {formattedDate}
+    //     </Typography>
+    //     <Typography variant="body2" color="text.secondary">
+    //       {BlogCardInfo.description}
+    //     </Typography>
+    //   </CardContent>
+    //   <CardActions>
+    //     <Button
+    //       sx={{
+    //         marginLeft: "auto",
+    //       }}
+    //       size="small"
+    //       onClick={viewBlogDetail}
+    //     >
+    //       Read More
+    //     </Button>
+    //   </CardActions>
+    // </Card>
   );
 }
